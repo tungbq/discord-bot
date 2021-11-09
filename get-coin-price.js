673999241ac3b86fbc335ddae2a012a74af12ac7
Tung Bui <tungbq.info@gmail.com>
2021-11-09 09:46:40 +0700

    Add get coin

---
+++ get-coin-price.js
@@ -0,0 +1,27 @@
+/* Example in Node.js ES6 using request-promise */
+
+require('dotenv').config();
+
+const rp = require('request-promise');
+const requestOptions = {
+	method: 'GET',
+	uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+	qs: {
+		start: '1',
+		limit: '20',
+		convert: 'USD',
+	},
+	headers: {
+		'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY,
+	},
+	json: true,
+	gzip: true,
+};
+
+rp(requestOptions)
+	.then((response) => {
+		console.log('API call response:', response);
+	})
+	.catch((err) => {
+		console.log('API call error:', err.message);
+	});
